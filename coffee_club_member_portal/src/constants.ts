4d8ee5d7182b040892defa6b0ccdcf791477b9e6
georgedigkas <george.digkas@mystenlabs.com>
2025-05-02 08:47:36 +0400

    update const

--- coffee_club_member_portal/src/constants.ts
+++ coffee_club_member_portal/src/constants.ts
@@ -4,5 +4,5 @@ export const COFFEE_CLUB_OBJECT_ID =
   '0x9e771ff31ca7bd391331810cf14b275f28b3a93525ea8104bd49fd02311bd833';
 // Define an array of pre-registered cafe IDs
 export const COFFEE_CLUB_CAFE_IDS = [
-  '0x225edbfe0f94596049263c2a5a0be4fe636e2507796635480d6366b50fd2d618',
+  '0xc116a2d5246408c7b03f25a9f9120fb93f9844bf437a7dbf132479d1effae194',
 ];
