1be2b95f1472f5e413b9155a70779041e4c86f22
georgedigkas <george.digkas@mystenlabs.com>
2025-06-16 21:24:09 +0300

    wip

--- coffee-club-nextjs/src/app/providers.tsx
+++ coffee-club-nextjs/src/app/providers.tsx
@@ -3,7 +3,7 @@
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { networkConfig } from "@/networkConfig";
+import { networkConfig } from "@/config/networkConfig";
 import { SuiClientProvider, WalletProvider } from "@mysten/dapp-kit";
 import { Theme } from "@radix-ui/themes";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
