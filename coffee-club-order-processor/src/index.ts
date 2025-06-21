b1a3a753aba0f6310ba9a298a80d4cc0cc0ae427
georgedigkas <george.digkas@mystenlabs.com>
2025-06-21 19:26:57 +0300

    fix issue

--- coffee-club-order-processor/src/index.ts
+++ coffee-club-order-processor/src/index.ts
@@ -4,7 +4,7 @@ import { Transaction } from "@mysten/sui/transactions";
 import { exec } from "child_process";
 import * as dotenv from "dotenv";
 import { promisify } from "util";
-import * as path from 'path';
+import * as path from "path";
 import { getAllOrders } from "./getAllOrders";
 
 dotenv.config({ path: "../.env" });
@@ -23,7 +23,10 @@ const CAFE_OWNER_ID = process.env.CAFE_OWNER_ID;
 const CAFE_MODULE = "suihub_cafe";
 const CHECK_INTERVAL_MS = 10_000;
 const PROCESSING_DURATION_MS = 120_000;
-const CONTROLLER_PATH = path.join(__dirname, '../../delonghi_controller/src/delonghi_controller.py');
+const CONTROLLER_PATH = path.join(
+  __dirname,
+  "../../delonghi_controller/src/delonghi_controller.py"
+);
 const MAC_ADDRESS = process.env.MAC_ADDRESS;
 
 if (!ADMIN_PHRASE) {
@@ -156,10 +159,12 @@ const pollAndProcessOrders = async () => {
           if (await processOrder(order.orderId)) {
             const coffeeType = order.coffeeType?.toLowerCase().trim();
             console.log("Sending Pythong Command");
-            console.log(`python3 ${CONTROLLER_PATH} ${MAC_ADDRESS} ${coffeeType}`);
-            // const { stdout, stderr } = await execAsync(
-            //   `python3 ${CONTROLLER_PATH} ${MAC_ADDRESS} ${coffeeType}`
-            // );
+            console.log(
+              `python3 ${CONTROLLER_PATH} ${MAC_ADDRESS} ${coffeeType}`
+            );
+            const { stdout, stderr } = await execAsync(
+              `python3 ${CONTROLLER_PATH} ${MAC_ADDRESS} ${coffeeType}`
+            );
             await delay(PROCESSING_DURATION_MS);
             await completeOrder(order.orderId);
           }
